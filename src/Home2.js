dd775f5e312b1fde253bc32f6af61ac942ce8959
sultanularefin <mhmdarefin@gmail.com>
2018-11-26 16:08:40 +0600

    xyzH2OOo

--- src/Home2.js
+++ src/Home2.js
@@ -1,8 +1,32 @@
-import React from 'react';
+import React, {Component} from 'react';
+
+import { Button } from 'react-bootstrap';
 
 class Home2 extends React.Component {
     render() {
-        return <h1>Hello, {this.props.name}</h1>;
+        return <h1>Hello, {" Arefin"}</h1>;
+
+        // return <Toolbar theme="dark" />;
     }
 }
+
+// function Toolbar(props) {
+//     // The Toolbar component must take an extra "theme" prop
+//     // and pass it to the ThemedButton. This can become painful
+//     // if every single button in the app needs to know the theme
+//     // because it would have to be passed through all components.
+//     return (
+//         <div>
+//             <ThemedButton theme={props.theme} />
+//         </div>
+//     );
+// }
+//
+// class ThemedButton extends React.Component {
+//     render() {
+//         return <Button theme={this.props.theme} bsStyle="primary" bsSize="large" active />;
+//     }
+// }
+
+
 export  default Home2;
