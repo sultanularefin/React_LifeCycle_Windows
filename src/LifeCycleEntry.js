c74cabec6d78d8401f3cba8ff54b95c64d183174
sultanul arefin <sultanul.arefin@nordecode.com>
2018-12-17 09:58:47 +0600

    rerer

--- src/LifeCycleEntry.js
+++ src/LifeCycleEntry.js
@@ -1,20 +1,19 @@
 import React, { Component } from 'react';
-// import ReactDom from 'react-dom';
-import Counter from './LifeCycleCounter';
-// import React, { Component } from 'react';
+
+// import Counter from './LifeCycleCounter';
+
 import logo from './logo.svg';
-// import Sidebar from "react-sidebar";
+
 import './App.css';
-// import Home2 from './Home2';
-// import RenderProps from './Renderprops';
 
 class LifeCycleEntry extends Component {
     render() {
         return (
             <div>
-                <Counter/>
-            </div>
-        );
+                {/*<Counter/>*/}
+
+                <h1> Hello world</h1>
+            </div>);
     }
 }
 
